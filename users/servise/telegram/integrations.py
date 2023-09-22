5891dad48a57f9653d6e129dd31932d5d101a508
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-09-22 23:45:33 +0300

    курсач по Django № 7

--- users/servise/telegram/integrations.py
+++ users/servise/telegram/integrations.py
@@ -3,7 +3,7 @@ import telebot
 
 def send_telegram_notification(habit):
     bot_token = '6441221156:AAHVLMCThPNM5Oz3cOs_3iS7Qt4lcW2exu0'
-    chat_id = 'YOUR_TELEGRAM_CHAT_ID'
+    chat_id = '1475370809'
     message = f"Reminder: {habit.name} - {habit.description}"
 
     bot = telebot.TeleBot(token=bot_token)
